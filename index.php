81b51fe1a73b06ba6d57adefb08553aa84dae985
pilotto000000002 <giacomopilotto3@gmail.com>
2019-02-19 10:09:57 +0100

    knb

--- index.php
+++ index.php
@@ -13,8 +13,22 @@
   <body>
     <h1>Hello, world!</h1>
     <p>ecco un paragrafo</p>
+    	
+    <?php 
 
-    <!-- Optional JavaScript -->
+    $arr = ["mela","kiwi","pera"];
+	for ($i=0; $i <count($arr);$i ++) { 
+	echo '<li>'. $arr[$i] . '</li>';
+
+	}
+
+	?>
+
+
+
+    
+
+ 	<!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
