1e94264e920d1d70a8477b62d3ec426963546dd1
Pilotto000000002 <giacomopilotto3@gmail.com>
2019-03-27 13:22:09 +0100

    lavoro

--- index.php
+++ index.php
@@ -8,27 +8,64 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
-    <title>Hello, world!</title>
+    <title></title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1></h1>
     <p>ecco un paragrafo</p>
     	
-    <?php 
+   
 
-    $arr = ["mela","kiwi","pera"];
-	for ($i=0; $i <count($arr);$i ++) { 
-	echo '<li>'. $arr[$i] . '</li>';
+	<?php 
 
-	}
-
+	  $nome = (isset($_POST['nome'])) ? $_['nome'] : '';
+	  $cognome = (isset($_POST['cognome'])) ? $_['cognome'] : '';
+	  $indirizzo = (isset($_POST['indirizzo'])) ? $_['indirizzo'] : '';
 	?>
 
+	<body>
+	  <div class="countainer">
+	 	<div class="alert alert-primary" role="alert">
+	 	  <?php  
+	 	  foreach ($_POST as $key => $value) {
+	 	  print $key . ':' . $value . '<br>';
+	 	  }
+	 	  ?>
+	 	</div>
 
+	  <?php if ($nome): ?>
+	  
+	  <p>condizione vera</p>
 
-    
+	  <?php else: ?>
+	  
+	  <p>condizione falsa</p>
 
- 	<!-- Optional JavaScript -->
+	  <?php endif; ?>	
+	 
+
+	 <form> 	
+	  
+	  <div class="form-group">
+   			 <label for="nome">Nome</label>
+    			<input type="text" class="form-control" id="nome" " placeholder="Inserisci il tuo nome">
+ 		 </div>
+  		
+  		 <div class="form-group">
+   			 <label for="cognome">Cognome</label>
+    			<input type="text" class="form-control" id="cognome"  placeholder="Inserisci il tuo cognome">
+ 		 </div>
+ 		 <div class="form-group">
+   			 <label for="indirizzo">Indirizzo</label>
+    			<input type="text" class="form-control" id="indirizzo" rows="5"></textarea>
+ 		 </div>
+ 		 	<button type="submit" class="btn btn-primary">invia</button>
+      </form>
+	
+	</body>  		
+	  	
+	  	
+<!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
